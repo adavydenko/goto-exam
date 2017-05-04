8db441bd48450884ea54a708b91f51261ae94832
adavydenko <alexandrdavydenko@yandex.ru>
2017-05-05 01:07:24 +0300

    Question service was added

---
+++ src/app/services/question.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from "@angular/http";
+import { Observable } from "rxjs/Rx";
+
+import { Question } from "./../models";
+
+@Injectable()
+export class QuestionService {
+
+  private apiUrl = 'http://localhost:3030/questions/';
+
+  constructor(private http: Http) { }
+
+  getQuestions(): Observable<Question[]> {
+    return this.http.get(this.apiUrl)
+      .map ( (data: Response) => data.json() )
+      .catch( (err:any) => Observable.throw(err.json().error || 'Server error') );
+  }
+
+}
